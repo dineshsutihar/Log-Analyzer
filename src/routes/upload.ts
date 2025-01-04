57d1f2e5de37220ed8dfca016f63ecec91a18559
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-01-04 20:50:49 +0530

    refactor the code to push coded in mongo db

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -4,6 +4,8 @@ import readline from 'readline';
 import multer from 'multer';
 import { parseSyslogLine } from '../utils/logParser';
 
+import {Log} from '../models/Log';
+
 const router = Router();
 
 const upload = multer({ dest: 'uploads/' });
@@ -20,10 +22,12 @@ router.post('/upload', upload.single('logfile'), (req: Request, res: Response): 
 
   const parsedLogs: any[] = [];
 
-  rl.on('line', (line: string) => {
+  rl.on('line', async(line: string) => {
     try {
       const parsedLog = parseSyslogLine(line);
-      parsedLogs.push(parsedLog);
+      const logEntry = new Log(parsedLog);
+      await logEntry.save();
+    parsedLogs.push(parsedLog);
     } catch (error) {
       console.error(`Failed to parse line: ${line}`);
     }
