c947471d5bda1e6ee8dc45441edd42c6c3d3f694
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-03-17 15:25:49 +0530

    wip: linux log get route is added

--- backend/src/routes/getAllLogs.ts
+++ backend/src/routes/getAllLogs.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import { WindowsLogModel } from '../models/LogWindowModel';
+import { LinuxLogModel } from '../models/LinuxLogModel';
 const app = express.Router();
 
-app.get('/logs', async (req, res) => {
+app.get('/winlogs', async (req, res) => {
   const logs = await WindowsLogModel.find().limit(100);
+  res.json(logs);
+});
+app.get('/linlogs', async (req, res) => {
+  const logs = await LinuxLogModel.find().limit(100);
   res.json(logs);
 });
 
