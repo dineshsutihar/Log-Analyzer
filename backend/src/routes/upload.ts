97797384871510c9f2c766cf53e2ae0c511ec7eb
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-03-17 15:39:54 +0530

    pef: removes some unused varibale and also removes unwanted try catch

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -14,33 +14,23 @@ router.post("/upload", upload.single("logfile"), async (req: Request, res: Respo
     return;
   }
 
-  const { hostname = "Unknown", source = "generic" } = req.body;
+  const { source = "generic" } = req.body;
   const fileName = req.file.originalname.toLowerCase();
   const fileBuffer = req.file.buffer;
 
-  try {
-    let logDocument;
-
-    if (fileName.endsWith(".csv")) {
-
-      logDocument = await parseWindowsEventLogCsv(fileBuffer);
-      const savedLog = await WindowsLogModel.insertMany(logDocument);
-
-    } else if (fileName.endsWith(".log")) {
-      const parseResult: LinuxLogModelType[] = await parseAllLinuxLog(fileBuffer.toString(), source);
-      await LinuxLogModel.insertMany(parseResult);
-    } else {
-      res.status(400).json({ error: "Unsupported file type" });
-      return;
-    }
-
-    res.json({ message: "Log successfully stored" });
-  } catch (error) {
-
-    console.error(error);
-    res.status(500).json({ error: "Failed to process the log file" });
+  if (fileName.endsWith(".csv")) {
+    let logDocument = await parseWindowsEventLogCsv(fileBuffer);
+    await WindowsLogModel.insertMany(logDocument);
+  } else if (fileName.endsWith(".log")) {
+    const parseResult: LinuxLogModelType[] = await parseAllLinuxLog(fileBuffer.toString(), source);
+    await LinuxLogModel.insertMany(parseResult);
+  } else {
+    res.status(400).json({ error: "Unsupported file type" });
     return;
   }
-});
+
+  res.json({ message: "Log successfully stored" });
+}
+);
 
 export default router;
