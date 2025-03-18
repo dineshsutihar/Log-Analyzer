4221c8b7283207aaaad497d9b94f15a334321997
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-03-18 15:33:39 +0530

    refactor: upload route is modified and choose os according to the extension of file

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,8 +1,9 @@
 import { Router, Request, Response } from "express";
 import multer from "multer";
-// import { parseSyslogLine } from "../utils/logParser";
+import { parseAllLinuxLog } from "../utils/logParser";
 import { parseWindowsEventLogCsv } from "../utils/parse-window";
 import { WindowsLogModel } from "../models/LogWindowModel";
+import { LinuxLogModel, LinuxLogModelType } from "../models/LinuxLogModel";
 
 const router = Router();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -21,42 +22,19 @@ router.post("/upload", upload.single("logfile"), async (req: Request, res: Respo
     let logDocument;
 
     if (fileName.endsWith(".csv")) {
+
       logDocument = await parseWindowsEventLogCsv(fileBuffer);
+      const savedLog = await WindowsLogModel.insertMany(logDocument);
 
-      // res.json({ message: "Log successfully stored", logId: logDocument._id }); 
-
-
-      // } else if (fileName.endsWith(".log")) {
-      //   const fileStream = Readable.from(fileBuffer);
-      //   const rl = readline.createInterface({ input: fileStream });
-
-      //   const entries: any[] = [];
-      //   for await (const line of rl) {
-      //     try {
-      //       // Pass source if needed for parsing
-      //       const parsedEntry = parseSyslogLine(line, source);
-      //       entries.push(parsedEntry);
-      //     } catch (error) {
-      //       console.error(`Failed to parse line: ${line}`);
-      //     }
-      //   }
-
-      //   logDocument = new LogModel({
-      //     logType: "linux",
-      //     logSubType: source,
-      //     hostname,
-      //     source,
-      //     entries,
-      //     uploadDate: new Date(),
-      //     analyzed: false,
-      //   });
+    } else if (fileName.endsWith(".log")) {
+      const parseResult: LinuxLogModelType[] = await parseAllLinuxLog(fileBuffer.toString(), source);
+      await LinuxLogModel.insertMany(parseResult);
     } else {
       res.status(400).json({ error: "Unsupported file type" });
       return;
     }
 
-    const savedLog = await WindowsLogModel.insertMany(logDocument);
-    res.json({ message: "Log successfully stored", logId: savedLog[0]._id });
+    res.json({ message: "Log successfully stored" });
   } catch (error) {
 
     console.error(error);
