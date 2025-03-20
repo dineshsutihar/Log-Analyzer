0bbbab7f26d6421b96341002068e0ff63bf3a1a5
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-03-20 13:46:55 +0530

    wip: adds generic [default] case to fetch unknow data

--- backend/src/utils/logParser.ts
+++ backend/src/utils/logParser.ts
@@ -2,6 +2,7 @@ import grok from 'grok-js';
 import { LinuxLogModel, LinuxLogModelType } from "../models/LinuxLogModel";
 import parseAuthLogFile from "./authlogParser";
 import parseKernelLogFile from './kernellogParser';
+import parseGenericLogs from './genericLinuxLogParser';
 
 export async function parseAllLinuxLog(data: string, source: string = "syslog"): Promise<any> {
 
@@ -31,29 +32,11 @@ export async function parseAllLinuxLog(data: string, source: string = "syslog"):
     //   }
     //   break;
     // }
-    // default: {
-    //   // Default Linux syslog format example:
-    //   // "Jun 14 15:16:01 combo sshd(pam_unix)[19939]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=218.188.2.4"
-    //   const syslogRegex = /^(\w+\s+\d+\s+\d+:\d+:\d+)\s+(\S+)\s+([\w\-.]+)(?:\(([\w\-.]+)\))?(?:\[(\d+)\])?:\s+(.*)$/;
-    //   const match = line.match(syslogRegex);
-    //   if (!match) {
-    //     throw new Error("Line does not match syslog format");
-    //   }
-    //   // Destructure the regex groups:
-    //   // Group 1: timestamp (e.g., "Jun 14 15:16:01")
-    //   // Group 2: hostname (e.g., "combo")
-    //   // Group 3: process name (e.g., "sshd")
-    //   // Group 4 (optional): process subtype (e.g., "pam_unix")
-    //   // Group 5 (optional): process id (e.g., "19939")
-    //   // Group 6: message (the remainder of the log line)
-    //   const [, ts, host, proc, procSub, pid, msg] = match;
-    //   timestamp = new Date(`${ts} ${new Date().getFullYear()}`);
-    //   eventId = proc;
-    //   processId = pid ? parseInt(pid, 10) : undefined;
-    //   message = msg;
-    //   severity = "INFO"; // default severity
-    //   break;
-    // }
+    default: {
+      // Default Linux Unknown format example:
+      // "Jun 14 15:16:01 combo sshd(pam_unix)[19939]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=218.188.2.4"
+      return parseGenericLogs(data);
+    }
 
   }
 
