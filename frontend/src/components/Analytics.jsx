5b428a3960ebe7fae3d568614c13c71e01a20676
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-03-26 03:12:33 +0530

    adds: adds analytics compoenet

---
+++ frontend/src/components/Analytics.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import CustomizedDataGrid from "./CustomizedDataGrid";
+
+export default function Analytics() {
+  return (
+    <Box sx={{ width: "100%", maxWidth: { sm: "100%", md: "1700px" } }}>
+      <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+        Details
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, lg: 12 }}>
+          <CustomizedDataGrid />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
